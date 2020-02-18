f6d7893dded04c0f77eb2a6763f0a802f112e1e8
sanjeet215 <sanjeet215@gmail.com>
2020-02-18 09:42:27 +0530

    File Management

--- src/main/java/com/asiczen/emp/mgmt/EmpMgmtApplication.java
+++ src/main/java/com/asiczen/emp/mgmt/EmpMgmtApplication.java
@@ -2,8 +2,12 @@ package com.asiczen.emp.mgmt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.asiczen.emp.mgmt.property.FileStorageProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({FileStorageProperties.class})
 public class EmpMgmtApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/asiczen/emp/mgmt/model/FileProp.java
@@ -0,0 +1,84 @@
+package com.asiczen.emp.mgmt.model;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "filetable")
+public class FileProp {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	Long id;
+	
+	@Column(nullable = false)
+	String fileName;
+	
+	@Column(nullable = false)
+	String filePath;
+	
+	@Column(nullable = false)
+	int size;
+	
+	@Column(nullable = false)
+	boolean active;
+
+	public FileProp() {
+		super();
+	}
+
+	public FileProp(Long id, String fileName, String filePath, int size, boolean active) {
+		super();
+		this.id = id;
+		this.fileName = fileName;
+		this.filePath = filePath;
+		this.size = size;
+		this.active = active;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+
+	public String getFilePath() {
+		return filePath;
+	}
+
+	public void setFilePath(String filePath) {
+		this.filePath = filePath;
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	public void setSize(int size) {
+		this.size = size;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+}
